e9c073658c1d77894e4714be0921e43acff015d2
Hackerr7767 <97411928+Hackerr7767@users.noreply.github.com>
2022-05-16 10:24:10 +0200

    The Kave package manager

---
+++ tools/kip.bat
@@ -0,0 +1,9 @@
+@echo off
+echo.
+echo The Kave Package Manager, Version 1.01
+echo.
+echo kip install <package-name>: Installs a package.
+echo.
+echo kip add <package-name>: Adds a package to the current project.
+echo.
+echo kip new <project-name>: Creates a new project.
